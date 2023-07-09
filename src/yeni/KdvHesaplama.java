0c2dad35045d19f65cfe00ef001542d93fe68870
batu han küçüktürk <bkucukturk@gmail.com>
2023-07-09 18:48:19 +0300

    kdv hesaplama programi tamamlandı

--- src/yeni/KdvHesaplama.java
+++ src/yeni/KdvHesaplama.java
@@ -1,4 +1,26 @@
 package yeni;
-
+import java.util.Scanner;
 public class KdvHesaplama {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double kdvsiz;
+        System.out.print("Lütfen Fiyat Giriniz :");
+        kdvsiz=inp.nextDouble();
+
+        //Eğer girilen tutar 0-1000 arasında ise KDV oranı %18, tutar 1000 TL'den büyük ise KDV oranı %8
+        double kdvOrani = (kdvsiz>0 && kdvsiz<=1000) ? 0.18 : 0.08;
+        double kdvTutari = (kdvsiz*kdvOrani);
+        double kdvli= (kdvsiz+kdvTutari);
+        System.out.println("KDV'siz tutar :"+kdvsiz+ "TL");
+        System.out.println("KDV'li tutar :"+kdvli+"TL");
+        System.out.println("KDV tutarı :"+kdvTutari+"TL");
+        System.out.println("KDV oranı :"+"%" + (kdvOrani*100));
+
+
+
+
+
+
+
+    }
 }
